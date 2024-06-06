e6a172ba79f90a079b4627727338ef2d004859e3
Joana Sthefanny Gomes Costa dos Santos <joanacosta015@gmail.com>
2024-06-06 14:02:34 -0300

    Main
    Main.css com as variáveis.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Home } from "./pages/Home";
+import About from "./components/about";
+// import { Home } from "./pages/Home";
 import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Home />
+    {/* <Home /> */}
+    <About />
   </React.StrictMode>
 );
