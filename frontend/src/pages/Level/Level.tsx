6b695adc930a60742bc9a79ca0079b479ed7127d
prsousa8 <prsousa.128@gmail.com>
2024-06-07 11:45:41 -0300

    feat: Add component Level

---
+++ frontend/src/pages/Level/Level.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useRef } from 'react';
+import image1 from '../../assets/home/perfil_tatu_bebe.png';
+import image2 from '../../assets/home/perfil_tatu_jovem.png';
+import image3 from '../../assets/home/perfil_tatu_adulto.png';
+import './Level.css';
+
+const Level: React.FC = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
+
+    const elements = containerRef.current?.querySelectorAll('.nivel-image-container');
+    elements?.forEach(element => observer.observe(element));
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+  
+
+  return (
+    <div className="nivel-container" ref={containerRef}>
+      <h2 className="nivel-title">Descubra o seu potencial</h2>
+      <p className="nivel-description">Adquira níveis de conhecimento com nossos conteúdos e desfrute da educação financeira conquistando novas habilidades ao longo da sua jornada!</p>
+      <div className="nivel-items">
+        <div className="nivel-item">
+        <label className="nivel-label"><h3>Tatu Bebê</h3></label>
+          <div className="nivel-image-container">
+            <img src={image1} alt="Imagem 1" className="nivel-image" />
+            <div className="nivel-overlay">
+              <p className="nivel-item-description">Parabéns! Você iniciou sua jornada de aprendizado. Nesse nível, você adquiriu noções básicas de educação financeira, bem como se familiarizou com a plataforma. Com isso, você é capaz de aplicar conceitos iniciais de forma acertada.</p>
+            </div>
+          </div>
+        </div>
+        <div className="nivel-item">
+        <label className="nivel-label"><h3>Tatu Jovem</h3></label>
+          <div className="nivel-image-container">
+            <img src={image2} alt="Imagem 2" className="nivel-image" />
+            <div className="nivel-overlay">
+              <p className="nivel-item-description">Você atingiu o segundo nível de conhecimento! Agora você é capaz de analisar suas finanças de forma satisfatória e elaborar planos detalhados para garantir sua saúde financeira, bem como de gerenciar o fluxo econômico de pequenas e médias empresas.</p>
+            </div>
+          </div>
+        </div>
+        <div className="nivel-item">
+        <label className="nivel-label"><h3>Tatu Adulto</h3></label>
+          <div className="nivel-image-container">
+            <img src={image3} alt="Imagem 3" className="nivel-image" />
+            <div className="nivel-overlay">
+              <p className="nivel-item-description">Viva! Agora você é expert em saúde financeira. Parabéns! Com isso, você é capaz de gerenciar a saúde financeira de grandes empresas, bem como de investir em ações das mais variadas naturezas. </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Level;
