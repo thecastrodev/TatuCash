a4851b0977e9276030c04de39bff69145daf29df
prsousa8 <prsousa.128@gmail.com>
2024-06-08 08:10:17 -0300

    refactor: update components styles

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,15 +1,11 @@
 import tatuLogo from "../../assets/tatucoin.png";
 import "./styles.css";
 import Rodape from "../../components/Footer/Rodape";
-import Header from "../../components/Header/Header";
-import Banner from "../../components/Banner/Banner";
 import Mission from "../../components/Mission/Mission";
 
 export function Home() {
   return (
     <>
-      <Header />
-      <Banner />
       <Mission />
       <Rodape />
     </>
