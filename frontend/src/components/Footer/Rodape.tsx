dd655336a32e202178901945c63fa57fd684569d
Joana Sthefanny Gomes Costa dos Santos <joanacosta015@gmail.com>
2024-06-08 19:28:04 -0300

    Merge remote-tracking branch 'origin/home' into About

---
+++ frontend/src/components/Footer/Rodape.tsx
@@ -0,0 +1,28 @@
+import instagramIcon from "../../assets/home/instagram.svg"; // Importe a imagem do ícone do Facebook
+import youtubeIcon from "../../assets/home/youtube.svg"; // Importe a imagem do ícone do YouTube
+import facebookIcon from "../../assets/home/facebook.svg"; // Importe a imagem do ícone do Instagram
+import logo from "../../assets/Logo/Logo_Tatucash_.svg"
+import './Rodape.css'
+
+const Rodape = () => {
+  return (
+    <footer className="rodape">
+      <div className="container">
+        <div className="logo_rodape">
+          <img src={logo} alt="Logo" />
+        </div>
+        <div className="frases">
+          <p>2024 Tatucash Soluções Financeiras LTDA</p>
+          <p>Todos os direitos reservados</p>
+        </div>
+        <div className="redes-sociais">
+        <img src={instagramIcon} alt="Instagram"/>
+        <img src={youtubeIcon} alt="YouTube" />
+        <div className="facebook"><img src={facebookIcon} alt="Facebook" /></div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Rodape;
